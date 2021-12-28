c0b9161e4eabed683b8d695efe61d9efe02b068d
Ricardo Meza <soy@ricardomeza.com>
2021-12-27 21:07:01 -0600

    Install Material UI
    - Install Material UI package
    - LInk MUI Fonts & Icons in public/index.html
    - Update Infosel UI Component Library Version

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,22 @@
+import { Button as ButtonRM } from '@ricardomeza/infosel-ui-component-library'
 import React from 'react'
 import { AppContextProvider } from '../state/appContext'
+import Button from '@mui/material/Button'
 
 const App = () => (
   <React.StrictMode>
     <AppContextProvider>
       <h1>Infosel Trading Criptocurrencies Dashboard</h1>
+      <p>
+        <ButtonRM label="UI Component Library Works!" />
+      </p>
+      <p>
+        <ButtonRM label="Small Button" size="small" />
+      </p>
+      <p>Material UI Button</p>
+      <p>
+        <Button variant="contained">Hello World</Button>
+      </p>
     </AppContextProvider>
   </React.StrictMode>
 )
